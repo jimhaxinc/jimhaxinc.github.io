6fe38141fbb9650738e39ef2f08144b586a31348
jimhaxinc <51718322+jimhaxinc@users.noreply.github.com>
2023-04-30 18:37:32 -0400

    Update CW.cpp

--- Comp1603-CWGenerator/CW.cpp
+++ Comp1603-CWGenerator/CW.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+#include <cmath>
 
 int calculateGrade(float* grades) {
-    return ((grades[0] / 100 * 0.06) + (grades[1] / 100 * 0.07) + (grades[2] / 50 * 0.07) + 
-            (grades[3] / 30 * 0.15) + (grades[4] / 65 * 0.15)) * 50;
+    return ceil(((grades[0] / 20 * 0.06) + (grades[1] / 25 * 0.06) + (grades[2] / 25 * 0.08) + 
+            (((grades[3] <= 26) ? grades[3] : 26) / 26 * 0.15) + (grades[4] / 30 * 0.15)) * 100);
 }
 
 int main() {
     float grades[5];
-    std::cout << "Enter Assignment 1 grade (out of 100): ";
+    std::cout << "Enter Assignment 1 grade (out of 20): ";
     std::cin >> grades[0];
-    std::cout << "Enter Assignment 2 grade (out of 100): ";
+    std::cout << "Enter Assignment 2 grade (out of 25): ";
     std::cin >> grades[1];
-    std::cout << "Enter Assignment 3 grade (out of 50): ";
+    std::cout << "Enter Assignment 3 grade (out of 25): ";
     std::cin >> grades[2];
     std::cout << "Enter Coursework 1 grade (out of 30): ";
     std::cin >> grades[3];
-    std::cout << "Enter Coursework 2 grade (out of 65): ";
+    std::cout << "Enter Coursework 2 grade (out of 30): ";
     std::cin >> grades[4];
 
     std::cout << "The final grade is: " << calculateGrade(grades) << std::endl;
